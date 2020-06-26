50755a7a25994eb8db70e5855168b83820ba31cf
Tamas Kurics <tamas.kurics@gmail.com>
2020-06-26 20:29:44 +0200

    reorganize simplification method storage class
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ src/main/scala/com/github/ghostrider77/simplification/CurveSimplifier.scala
@@ -0,0 +1,149 @@
+package com.github.ghostrider77.simplification
+
+import scala.annotation.tailrec
+import scala.collection.mutable.{PriorityQueue => Heap}
+
+final case class Point(x: Double, y: Double)
+
+class CurveSimplifier(points: Vector[Point]) {
+  import CurveSimplifier.{perpendicularDistanceSimplification,
+                          radialDistanceSimplification,
+                          modifiedDPSimplification,
+                          dPSimplification}
+
+  private val nrPoints: Int = points.length
+  private val isTrivial: Boolean = nrPoints <= 2
+
+  private def edgeCase(n: Int): Option[Vector[Point]] = {
+    if (n <= 0 || points.isEmpty) Some(Vector())
+    else if (n == 1) Some(Vector(points(0)))
+    else if (n == 2 && nrPoints >= 2) Some(Vector(points(0), points(nrPoints - 1)))
+    else if (n >= nrPoints) Some(points)
+    else None
+  }
+
+  def modifiedDouglasPeucker(n: Int): Vector[Point] = edgeCase(n) match {
+    case None => modifiedDPSimplification(points, nrPoints, n)
+    case Some(result) => result
+  }
+
+  def douglasPeucker(epsilon: Double): Vector[Point] =
+    if (isTrivial) points else dPSimplification(points, epsilon)
+
+  def radialDistance(epsilon: Double): Vector[Point] =
+    if (isTrivial) points else radialDistanceSimplification(points, nrPoints, epsilon)
+
+  def perpendicularDistance(epsilon: Double): Vector[Point] =
+    if (isTrivial) points else perpendicularDistanceSimplification(points, nrPoints, epsilon)
+}
+
+object CurveSimplifier {
+  private type Index = Int
+
+  private def radialDistanceSimplification(points: Vector[Point], nrPoints: Int, epsilon: Double): Vector[Point] = {
+    require(nrPoints >= 1)
+
+    @tailrec
+    def collectKeyPoints(keyPoints: List[Point], currentKeyPoint: Point, ix: Index): Vector[Point] = {
+      val point: Point = points(ix)
+      if (ix == nrPoints - 1) (point :: keyPoints).reverse.toVector
+      else {
+        val distance: Double = euclideanDistance(currentKeyPoint, point)
+        if (distance >= epsilon) collectKeyPoints(point :: keyPoints, point, ix + 1)
+        else collectKeyPoints(keyPoints, currentKeyPoint, ix + 1)
+      }
+    }
+
+    val initialKeyPoint: Point = points(0)
+    collectKeyPoints(List(initialKeyPoint), initialKeyPoint, ix = 1)
+  }
+
+  private def perpendicularDistanceSimplification(points: Vector[Point],
+                                                  nrPoints: Int,
+                                                  epsilon: Double): Vector[Point] = {
+    require(nrPoints >= 2)
+
+    @tailrec
+    def collectKeyPoints(keyPoints: List[Point], leftEndPoint: Point, ix: Index): Vector[Point] = {
+      if (ix >= nrPoints - 1) {
+        val lastPoint: Point = points(nrPoints - 1)
+        (lastPoint :: keyPoints).reverse.toVector
+      }
+      else {
+        val point: Point = points(ix)
+        val rightEndpoint: Point = points(ix + 1)
+        val distance: Double = distanceFromSegment(point, leftEndPoint, rightEndpoint)
+        if (distance >= epsilon) collectKeyPoints(point :: keyPoints, point, ix + 1)
+        else collectKeyPoints(rightEndpoint :: keyPoints, rightEndpoint, ix + 2)
+      }
+    }
+
+    val initialKeyPoint: Point = points(0)
+    collectKeyPoints(List(initialKeyPoint), initialKeyPoint, ix = 1)
+  }
+
+  private def dPSimplification(points: Vector[Point], epsilon: Double): Vector[Point] = {
+    ???
+  }
+
+  private def modifiedDPSimplification(points: Vector[Point], nrPoints: Int, n: Int): Vector[Point] = {
+    require(n >= 2)
+    val heap: Heap[(Double, Index)] = Heap((Double.PositiveInfinity, 0), (Double.PositiveInfinity, nrPoints - 1))
+
+    @tailrec
+    def loop(stack: List[(Index, Index)]): Unit = stack match {
+      case Nil => ()
+      case (startIx, endIx) :: rest if endIx > startIx + 1 =>
+        val (maxDistance, maxIndex): (Double, Index) = calcMaximalDistanceInSegment(points, startIx, endIx)
+        heap.enqueue((maxDistance, maxIndex))
+        loop((maxIndex, endIx) :: (startIx, maxIndex) :: rest)
+      case _ :: rest => loop(rest)
+    }
+
+    loop(List((0, nrPoints - 1)))
+    val indices: Vector[Index] = extractMaxDistanceIndices(heap, n)
+    indices.map(points)
+  }
+
+  private def calcMaximalDistanceInSegment(points: Vector[Point], startIx: Index, endIx: Index): (Double, Index) = {
+    val leftEndPoint: Point = points(startIx)
+    val rightEndPoint: Point = points(endIx)
+
+    @tailrec
+    def visitPointsInSegment(maxDistance: Double, maxIndex: Index, ix: Int): (Double, Index) = {
+      if (ix >= endIx) (maxDistance, maxIndex)
+      else {
+        val point: Point = points(ix)
+        val distance: Double = distanceFromSegment(point, leftEndPoint, rightEndPoint)
+        if (distance > maxDistance) visitPointsInSegment(distance, ix, ix + 1)
+        else visitPointsInSegment(maxDistance, maxIndex, ix + 1)
+      }
+    }
+
+    visitPointsInSegment(-1.0, startIx, startIx + 1)
+  }
+
+  private def extractMaxDistanceIndices(heap: Heap[(Double, Index)], size: Int): Vector[Index] = {
+    require(heap.size >= size)
+    @tailrec
+    def nLargest(acc: List[Index], n: Int): Vector[Index] = {
+      if (n >= size) acc.sorted.toVector
+      else {
+        val (_, ix): (Double, Index) = heap.dequeue()
+        nLargest(ix :: acc, n + 1)
+      }
+    }
+    nLargest(Nil, 0)
+  }
+
+  private def distanceFromSegment(p: Point, q1: Point, q2: Point): Double = {
+    if (q1.x == q2.x) math.abs(p.x - q1.x)
+    else {
+      val slope: Double =  (q2.y - q1.y) / (q2.x - q1.x)
+      val intercept: Double = q1.y - slope * q1.x
+      math.abs(slope * p.x - p.y + intercept) / math.sqrt(math.pow(slope, 2) + 1)
+    }
+  }
+
+  private def euclideanDistance(p: Point, q: Point): Double = math.hypot(p.x - q.x, p.y - q.y)
+}
--- src/test/scala/com/github/ghostrider77/simplification/CurveSimplifierSuite.scala
+++ src/test/scala/com/github/ghostrider77/simplification/CurveSimplifierSuite.scala
@@ -3,9 +3,7 @@ package com.github.ghostrider77.simplification
 import org.scalatest.{FreeSpec, Inspectors, Matchers, PrivateMethodTester}
 import scala.collection.mutable.{PriorityQueue => Heap}
 
-class SimplificationSuite extends FreeSpec with Matchers with Inspectors with PrivateMethodTester {
-  import com.github.ghostrider77.simplification.Simplification.Point
-
+class CurveSimplifierSuite extends FreeSpec with Matchers with Inspectors with PrivateMethodTester {
   val tolerance: Double = 1e-8
 
   "DistanceFromSegment" - {
@@ -16,19 +14,19 @@ class SimplificationSuite extends FreeSpec with Matchers with Inspectors with Pr
       "distance from horizontal line is calculated" in {
         val p: Point = Point(5, 2)
         val q: Point = Point(6, 0)
-        Simplification invokePrivate distanceFromSegment(p, origin, q) should be (2.0 +- tolerance)
+        CurveSimplifier invokePrivate distanceFromSegment(p, origin, q) should be (2.0 +- tolerance)
       }
 
       "point is outside of the segment" in {
         val p: Point = Point(10, 2)
         val q: Point = Point(6, 0)
-        Simplification invokePrivate distanceFromSegment(p, origin, q) should be (2.0 +- tolerance)
+        CurveSimplifier invokePrivate distanceFromSegment(p, origin, q) should be (2.0 +- tolerance)
       }
 
       "the point is in general position with respect to the segment" in {
-        val d1: Double = Simplification invokePrivate distanceFromSegment(Point(3, 3), origin, Point(4, 2))
-        val d2: Double = Simplification invokePrivate distanceFromSegment(Point(3, 8), origin, Point(4, 2))
-        val d3: Double = Simplification invokePrivate distanceFromSegment(Point(1, 3), Point(0, 2), Point(2, 0))
+        val d1: Double = CurveSimplifier invokePrivate distanceFromSegment(Point(3, 3), origin, Point(4, 2))
+        val d2: Double = CurveSimplifier invokePrivate distanceFromSegment(Point(3, 8), origin, Point(4, 2))
+        val d3: Double = CurveSimplifier invokePrivate distanceFromSegment(Point(1, 3), Point(0, 2), Point(2, 0))
 
         d1 shouldBe ((1 / 5.0) * math.sqrt(45) +- tolerance)
         d2 shouldBe ((1 / 5.0) * math.sqrt(845) +- tolerance)
@@ -44,13 +42,13 @@ class SimplificationSuite extends FreeSpec with Matchers with Inspectors with Pr
       val heap: Heap[(Double, Int)] =
         Heap((10.0, 0), (8.0, 1), (0.0, 2), (1.0, 3), (0.5, 4), (11.0, 5), (7.0, 6), (0.0, 7), (15.0, 8), (2.0, 9))
 
-      val indices: Vector[Int] = Simplification invokePrivate extractMaxDistanceIndices(heap, 4)
+      val indices: Vector[Int] = CurveSimplifier invokePrivate extractMaxDistanceIndices(heap, 4)
       indices should contain theSameElementsAs Vector(8, 5, 0, 1)
     }
 
     "Should throw an exception if the number of points is less than the requested size of the simplified curve" in {
       val heap: Heap[(Double, Int)] = Heap((1.0, 0))
-      an[IllegalArgumentException] should be thrownBy (Simplification invokePrivate extractMaxDistanceIndices(heap, 2))
+      an[IllegalArgumentException] should be thrownBy (CurveSimplifier invokePrivate extractMaxDistanceIndices(heap, 2))
     }
   }
 
@@ -63,33 +61,9 @@ class SimplificationSuite extends FreeSpec with Matchers with Inspectors with Pr
       val startIx: Int = 1
       val endIx: Int = 5
       val (maxDistance, maxIndex): (Double, Int) =
-        Simplification invokePrivate calcMaximalDistanceInSegment(points, startIx, endIx)
+        CurveSimplifier invokePrivate calcMaximalDistanceInSegment(points, startIx, endIx)
       maxIndex shouldEqual 4
       maxDistance should be (2.0 +- tolerance)
-    }
-  }
-
-  "Simplify" - {
-    import com.github.ghostrider77.simplification.Simplification.simplify
-
-    "Should handle all edge cases" in {
-      simplify(Vector(), -1) shouldBe empty
-      simplify(Vector(), 1) shouldBe empty
-      simplify(Vector(), 10) shouldBe empty
-      simplify(Vector(Point(0, 0)), 10) shouldEqual Vector(Point(0, 0))
-      simplify(Vector(Point(0, 0), Point(0, 1), Point(3, 4)), 10) should have length 3
-      simplify(Vector(Point(0, 0), Point(0, 1), Point(3, 4)), 1) should have length 1
-    }
-
-    "Should simplify a curve" in {
-      val points: Vector[Point] = Vector(Point(0, 0), Point(1, 1.5), Point(2, 2), Point(3, 1), Point(4, 0))
-      val result: Vector[Point] = simplify(points, 4)
-      val expected: Vector[Point] = Vector(Point(0, 0), Point(1, 1.5), Point(2, 2), Point(4, 0))
-      forAll (result.zip(expected)) {
-        case (Point(a, b), Point(x, y)) =>
-          a shouldBe (x +- tolerance)
-          b shouldBe (y +- tolerance)
-      }
     }
   }
 }
